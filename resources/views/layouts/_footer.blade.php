aa1e9111fe42c984b061c3cb7b55f813d730affd
superman <13706831394@163.com>
2022-08-29 13:15:13 +0000

    样式修改

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
   <div class="container">
     <p class="float-start">
-      由 <a href="http://weibo.com/u/1837553744?is_hot=1" target="_blank">Super_coder</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+      由 <a href="https://weibo.com/5495251502/profile?topnav=1&wvr=6" target="_blank">Super_coder</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
     </p>
 
     <p class="float-end"><a href="mailto:13706831394@163.com">联系我们</a></p>
