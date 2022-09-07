58baa2015171ec724b176e14c4e8164c931bd6b5
superman <13706831394@163.com>
2022-09-07 13:36:28 +0000

    编辑个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -13,13 +14,14 @@ class UsersController extends Controller
     }
 
     //用户信息编辑表单
-    public function edit() {
-
+    public function edit(User $user) {
+        return view('users.edit', compact('user'));
     }
 
     //用户信息修改
-    public function update() {
-
+    public function update(UserRequest $request, User $user) {
+        $user->update($request->all());
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料已更新！');
     }
 
 
