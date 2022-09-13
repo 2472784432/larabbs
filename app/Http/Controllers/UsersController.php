88a73d38211da5cbcaf009f00f5b1c0248a0a2e8
superman <13706831394@163.com>
2022-09-13 13:42:05 +0000

    授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,10 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
+    public function __construct() {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+
     //用户信息展示
     public function show(User $user) {
         return view('users.show', compact('user'));
@@ -16,11 +20,14 @@ class UsersController extends Controller
 
     //用户信息编辑表单
     public function edit(User $user) {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     //用户信息修改
     public function update(UserRequest $request, ImageUploadHandler $uploader, User $user) {
+
+        $this->authorize('update', $user);
         $data = $request->all();
         if ($request->avatar) {
             $result = $uploader->save($request->avatar, 'avatars', $user->id, 416);
