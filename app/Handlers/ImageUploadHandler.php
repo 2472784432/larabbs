9027ec477cd4994e58a8040da18a280b91c3641f
superman <13706831394@163.com>
2022-09-13 13:33:19 +0000

    裁剪头像

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -5,12 +5,21 @@ namespace App\Handlers;
 
 
 use Illuminate\Support\Str;
+use Intervention\Image\Facades\Image;
 
 class ImageUploadHandler
 {
     protected $allowedExt = ['png', 'jpg', 'gif', 'jpeg'];
 
-    public function save($file, $folder, $filePrefix) {
+    /**
+     * 图片保存
+     * @param $file
+     * @param $folder
+     * @param $filePrefix
+     * @param bool $maxWidth
+     * @return bool|string[]
+     */
+    public function save($file, $folder, $filePrefix, $maxWidth = false) {
         //文件保存路径(相对路径)
         $folderName = "uploads/images/$folder/" . date('Ym/d', time());
 
@@ -27,8 +36,33 @@ class ImageUploadHandler
             return false;
         }
 
+        //移动到目标存储路径
         $file->move($uploadPath, $fileName);
 
+        //如果限制了图片大小 就按要求裁剪
+        if ($maxWidth && $extension != 'gif') {
+            $this->reduceSize($uploadPath . '/' . $fileName, $maxWidth);
+        }
+
         return ['path' => config('app.url') . "/$folderName/$fileName"];
+    }
+
+    /**
+     * 裁剪图片大小
+     * @param $filePath
+     * @param $maxWidth
+     */
+    public function reduceSize($filePath, $maxWidth) {
+        //实例化
+        $image = Image::make($filePath);
+
+        //照片大小调整
+        $image->resize($maxWidth, $maxWidth, function ($constraint) {
+            $constraint->aspectRatio();
+            $constraint->upsize();
+        });
+
+        //保存
+        $image->save();
     }
 }
